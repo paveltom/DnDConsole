9d271e5b7d177426034ab5f1c4441783ca1c1a13
paveltom <paveltom@post.bgu.ac.il>
2020-06-13 10:03:16 +0300

    opened a repository

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -2,5 +2,21 @@ public class Coordinate {
     private int x;
     private int y;
 
+    public Coordinate(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    //getleft
+    public int getolumnCoordinate(){ return this.x;}
+    public int getRowCoordinate(){ return this.y;}
+
+    public String ToString(){
+        return "<"+x+","+y+">";
+    }
+
+    //getright
+    //tostring
+
 
 }
