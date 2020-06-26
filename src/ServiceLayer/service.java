5640cd47bd93cdd31bca161c08e826a8b36cee97
nitay vitkin <nitayv@post.bgu.ac.il>
2020-06-26 09:42:21 +0300

    newLevel - service

--- src/ServiceLayer/service.java
+++ src/ServiceLayer/service.java
@@ -1,11 +1,13 @@
 package ServiceLayer;
 
 import BusinessLayer.BoardPackage.Board;
+import BusinessLayer.TilesPackage.Units.Players.Player;
 import BusinessLayer.TilesPackage.Units.Unit;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Scanner;
 
 public class service {
@@ -14,6 +16,7 @@ public class service {
     private String input;
     private Scanner scan;
     private String [][] level;
+    private String[] args;
 
     public service()
     {
@@ -21,6 +24,7 @@ public class service {
         b = new Board();
     }
     public void newgame(String[] args) throws FileNotFoundException {
+        this.args=args;
         level =readfile(args);
         System.out.println(b.getPlayerSelection());
         selecetplayer();
@@ -42,48 +46,78 @@ public class service {
         b.crateBoard(level,Integer.parseInt(input));
         System.out.println("You Selected:"+'\n'+b.getCurrPlayer().Name);
     }
-    public void gameTick ()
-    {
+    public void gameTick () throws FileNotFoundException {
         input="";
         input = scan.nextLine();
         if (!(input.equals("w") |input.equals("s")|input.equals("a")|input.equals("d")|input.equals("e")|input.equals("q"))) {
             System.out.println("Select a valid actin or movement command");
             gameTick();
         }
-        b.gameTick(input);
+        List<String> output = b.gameTick(input);
+        for(String run:output) {
+            System.out.println(run);
+        }
+        if(b.PlayerAlive()){////////////////////// necessary???
+            System.out.println(b.toString());
+            if(b.getEnemySize()==0)
+                nextLevel(args);
+            gameTick();
+        }
+    }
+    private void nextLevel(String[] args) throws FileNotFoundException {
+        if(b.getEnemySize()==0) {
+            System.out.println("Load next Level");
+            Player CurrPlayer = b.getCurrPlayer();
+            level = readfile(args);
+            b.crateBoard(level, 1);
+            b.setCurrPlayer(CurrPlayer);
+            for (int a = 0; a < level.length; a++) {
+                for (int j = 0; j < level[a].length; j++)
+                    System.out.print(level[a][j]);
+                System.out.println();
+            }
+            System.out.println(b.getCurrPlayer().actualStats());
+        }
     }
     private String [][] readfile(String[] args) throws FileNotFoundException {
         String target_dir = "levels/";
         File dir = new File(target_dir);
         File[] files = dir.listFiles();
-        File test = files[index];
-        if(!test.exists()) System.out.println(test.getAbsolutePath());/////////////////////
-        int i=0;
-        Scanner myscanner = new Scanner(test);
-        Scanner run1 =  new Scanner(test);
-        while (run1.hasNextLine()){ run1.nextLine(); i++;}
-        Scanner run = new Scanner(test);
-        String [][] level=new String[i][];
-        String temp = "";
-        int k=0;
-        while(run.hasNextLine()) {
-            temp = run.nextLine();
-            String[] trans;
-            trans=Translate(temp);
-            level[k] = trans;
-            k++;
+        if(index<files.length) {
+            File Currlevel = files[index];
+            if (!Currlevel.exists()) System.out.println(Currlevel.getAbsolutePath());/////////////////////
+            int i = 0;
+            Scanner myscanner = new Scanner(Currlevel);
+            Scanner run1 = new Scanner(Currlevel);
+            while (run1.hasNextLine()) {
+                run1.nextLine();
+                i++;
+            }
+            Scanner run = new Scanner(Currlevel);
+            String[][] level = new String[i][];
+            String Currline = "";
+            int k = 0;
+            while (run.hasNextLine()) {
+                Currline = run.nextLine();
+                String[] trans;
+                trans = Translate(Currline);
+                level[k] = trans;
+                k++;
+            }
+            index++;
+            return level;
         }
-        index++;
-        return level;
+        System.out.println("YOU WON, Congratulations!");
+        return null;
     }
-    private String [] Translate (String c)
+    private String [] Translate (String Currline)
     {
         int k=0;
-        String [] output=new String[c.length()];
+        String [] output=new String[Currline.length()];
         String temp = "@#.skqzbgwMCKBQD";
-        for(int i=0;i<c.length();i++)
+        for(int i=0;i<Currline.length();i++)
         {
-            k=temp.indexOf(c.charAt(i));
+            k=temp.indexOf(Currline.charAt(i));
             output[i]=temp.charAt(k)+"";
         }
         return output;
